a8ad9d89735e6a243bc9751212045489f9be7b07
Niro-jpg <nicolodentale@gmail.com>
2023-03-25 16:18:52 +0100

    deleted folders

--- Classification/testst.py
+++ Classification/testst.py
@@ -6,23 +6,23 @@ from VARIABLES import *
 import chardet
 import torchtext
 
-with open(ARCHIVE_PATH, 'rb') as rawdata:
-    result = chardet.detect(rawdata.read(100000))
-print(result)
+#with open(ARCHIVE_PATH, 'rb') as rawdata:
+#    result = chardet.detect(rawdata.read(100000))
+#print(result)
 
-data = pd.read_csv(ARCHIVE_PATH)
+#data = pd.read_csv(ARCHIVE_PATH)
 
-features = data.iloc[:,:1]
-target = data.iloc[:,-1:]
+#features = data.iloc[:,:1]
+#target = data.iloc[:,-1:]
 
 #features_tensor = torch.tensor(features.values)
 
-label_encoder = LabelEncoder()
-target_int = label_encoder.fit_transform(target)
-target_tensor = torch.tensor(target_int)
+#label_encoder = LabelEncoder()
+#target_int = label_encoder.fit_transform(target)
+#target_tensor = torch.tensor(target_int)
 
 #print(features)
-print(target_tensor)
+#print(target_tensor)
 
 tokenizer = torchtext.data.utils.get_tokenizer('basic_english')
-text_field = torchtext.data.Field(sequential=True, use_vocab=True, tokenize=tokenizer, lower=True, batch_first=True)
+text_field = torchtext.legacy.data.utils.Field(sequential=True, use_vocab=True, tokenize=tokenizer, lower=True, batch_first=True)
